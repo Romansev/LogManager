f177ebf2f82384cd8d85a3e4fe640c6a71cf7723
Romansev <ravlasov@spasibosb.ru>
2021-07-04 19:58:38 +0300

    ref

--- src/app/Http/Controllers/LogController.php
+++ src/app/Http/Controllers/LogController.php
@@ -4,9 +4,55 @@ namespace Backpack\LogManager\app\Http\Controllers;
 
 use Backpack\LogManager\app\Classes\LogViewer;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class LogController extends Controller
 {
+    private static $levels_classes = [
+        'debug'     => 'info',
+        'info'      => 'info',
+        'notice'    => 'info',
+        'warning'   => 'warning',
+        'error'     => 'danger',
+        'critical'  => 'danger',
+        'alert'     => 'danger',
+        'emergency' => 'danger',
+        'processed' => 'info',
+    ];
+
+    /**
+     * Map debug levels to icon classes.
+     *
+     * @var array
+     */
+    private static $levels_imgs = [
+        'debug'     => 'info',
+        'info'      => 'info',
+        'notice'    => 'info',
+        'warning'   => 'warning',
+        'error'     => 'warning',
+        'critical'  => 'warning',
+        'alert'     => 'warning',
+        'emergency' => 'warning',
+        'processed' => 'info',
+    ];
+
+    /**
+     * Log levels that are used.
+     *
+     * @var array
+     */
+    private static $log_levels = [
+        'emergency',
+        'alert',
+        'critical',
+        'error',
+        'warning',
+        'notice',
+        'info',
+        'debug',
+        'processed',
+    ];
     /**
      * Lists all log files.
      *
@@ -14,7 +60,7 @@ class LogController extends Controller
      */
     public function index()
     {
-        $this->data['files'] = LogViewer::getFiles(true, true);
+        $this->data['files'] = $this->getFiles();
         $this->data['title'] = trans('backpack::logmanager.log_manager');
 
         return view('logmanager::logs', $this->data);
@@ -27,9 +73,10 @@ class LogController extends Controller
      */
     public function preview($file_name)
     {
-        LogViewer::setFile(decrypt($file_name));
+        $logsPath = storage_path('logs');
+        $file = $logsPath.'/'.decrypt($file_name);
 
-        $logs = LogViewer::all();
+        $logs = $this->getLog($file);
 
         if (count($logs) <= 0) {
             abort(404, trans('backpack::logmanager.log_file_doesnt_exist'));
@@ -39,7 +86,7 @@ class LogController extends Controller
         $this->data['title'] = trans('backpack::logmanager.preview').' '.trans('backpack::logmanager.logs');
         $this->data['file_name'] = decrypt($file_name);
 
-        return view('logmanager::log_item', $this->data);
+        return view('log_item', $this->data);
     }
 
     /**
@@ -76,5 +123,89 @@ class LogController extends Controller
         }
 
         abort(404, trans('backpack::logmanager.log_file_doesnt_exist'));
+    }
+
+    private function getLog($file)
+    {
+        $log = [];
+
+
+        $file = app('files')->get($file);
+
+        $pattern = '/\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{6}\].*/';
+
+        preg_match_all($pattern, $file, $headings);
+
+        if (!is_array($headings)) {
+            return $log;
+        }
+
+        $stack_trace = preg_split($pattern, $file);
+
+        if ($stack_trace[0] < 1) {
+            array_shift($stack_trace);
+        }
+
+        foreach ($headings as $h) {
+            $h = array_slice($h, -10);
+            for ($i = 0, $j = count($h); $i < $j; $i++) {
+                foreach (static::$log_levels as $level) {
+                    if (strpos(strtolower($h[$i]), '.'.$level) || strpos(strtolower($h[$i]), $level.':')) {
+                        $pattern = '/^\[(?P<date>(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{6}))\](?:.*?(?P<context>(\w+))\.|.*?)'.$level.': (?P<text>.*?)(?P<in_file> in .*?:[0-9]+)?$/i';
+                        preg_match($pattern, $h[$i], $current);
+                        if (!isset($current['text'])) {
+                            continue;
+                        }
+
+                        $jsonStart = strpos($current['text'], '{');
+
+                        $text = substr($current['text'], $jsonStart);
+
+                        $log[] = [
+                            'context'     => $current['context'],
+                            'level'       => $level,
+                            'level_class' => static::$levels_classes[$level],
+                            'level_img'   => static::$levels_imgs[$level],
+                            'date'        => $current['date'],
+                            'text'        => $text,
+                            'in_file'     => isset($current['in_file']) ? $current['in_file'] : null,
+                            'stack'       => ''
+                        ];
+                    }
+                }
+            }
+        }
+
+        return array_reverse($log);
+    }
+
+    private function getFiles()
+    {
+        $filesAnswers = glob(storage_path().'/logs/*ANSWERS*.log');
+        $filesAnswers = array_slice($filesAnswers, -3);
+        $filesErrors = glob(storage_path().'/logs/*TRAVEL_API*.log');
+        $filesErrors = array_slice($filesErrors, -3);
+        $files = array_merge($filesAnswers, $filesErrors);
+
+        $files = array_reverse($files);
+        $files = array_filter($files, 'is_file');
+
+        if (is_array($files)) {
+            foreach ($files as $k => $file) {
+                $disk = Storage::disk(config('backpack.base.root_disk_name'));
+                $file_name = basename($file);
+
+                if ($disk->exists('storage/logs/'.$file_name)) {
+                    $files[$k] = [
+                        'file_name'     => $file_name,
+                        'file_size'     => $disk->size('storage/logs/'.$file_name),
+                        'last_modified' => $disk->lastModified('storage/logs/'.$file_name),
+                    ];
+                }
+            }
+        }
+
+        return array_values($files);
+
     }
 }
--- src/resources/views/log_item.blade.php
+++ src/resources/views/log_item.blade.php
@@ -1,51 +1,83 @@
 @extends(backpack_view('layouts.top_left'))
 
 @php
-  $breadcrumbs = [
-    trans('backpack::crud.admin') => backpack_url('dashboard'),
-    trans('backpack::logmanager.log_manager') => backpack_url('log'),
-    trans('backpack::logmanager.preview') => false,
-  ];
+    $breadcrumbs = [
+      trans('backpack::crud.admin') => backpack_url('dashboard'),
+      trans('backpack::logmanager.log_manager') => backpack_url('log'),
+      trans('backpack::logmanager.preview') => false,
+    ];
 @endphp
 
 @section('header')
     <section class="container-fluid">
-      <h2>
-        {{ trans('backpack::logmanager.log_manager') }}<small>{{ trans('backpack::logmanager.file_name') }}: <i>{{ $file_name }}</i></small>
-        <small><a href="{{ backpack_url('log') }}" class="hidden-print font-sm"><i class="la la-angle-double-left"></i> {{ trans('backpack::logmanager.back_to_all_logs') }}</a></small>
-      </h2>
+        <h2>11
+            {{ trans('backpack::logmanager.log_manager') }}<small>{{ trans('backpack::logmanager.file_name') }}: <i>{{ $file_name }}</i></small>
+            <small><a href="{{ backpack_url('log') }}" class="hidden-print font-sm"><i class="la la-angle-double-left"></i> {{ trans('backpack::logmanager.back_to_all_logs') }}</a></small>
+        </h2>
     </section>
 @endsection
 
 @section('content')
-  <div id="accordion" role="tablist" aria-multiselectable="true">
-    @forelse($logs as $key => $log)
-      <div class="card mb-0 pb-0">
-        <div class="card-header bg-{{ $log['level_class'] }}" role="tab" id="heading{{ $key }}">
-            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $key }}" aria-expanded="true" aria-controls="collapse{{ $key }}" class="text-white">
-              <i class="la la-{{ $log['level_img'] }}"></i>
-              <span>[{{ $log['date'] }}]</span>
-              {{ Str::limit($log['text'], 150) }}
-            </a>
-        </div>
-        <div id="collapse{{ $key }}" class="panel-collapse collapse p-3" role="tabpanel" aria-labelledby="heading{{ $key }}">
-          <div class="panel-body">
-            <p>{{$log['text']}}</p>
-            <pre><code class="php">
+    <div id="accordion" role="tablist" aria-multiselectable="true">
+        @forelse($logs as $key => $log)
+            <div class="card mb-0 pb-0">
+                <div class="card-header bg-{{ $log['level_class'] }}" role="tab" id="heading{{ $key }}">
+                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $key }}" aria-expanded="true" aria-controls="collapse{{ $key }}" class="text-white">
+                        <i class="la la-{{ $log['level_img'] }}"></i>
+                        <span>[{{ $log['date'] }}]</span>
+                        {{ Str::limit($log['text'], 150) }}
+                    </a>
+                </div>
+                <div id="collapse{{ $key }}" class="panel-collapse collapse p-3" role="tabpanel" aria-labelledby="heading{{ $key }}">
+                    <div class="panel-body">
+                        {{--            <p>{{$log['text']}}</p>--}}
+                        <div id="jsoneditor-{{ $key }}" style="height: 400px;"></div>
+
+                        <pre><code class="php">
               {{ trim($log['stack']) }}
             </code></pre>
-          </div>
-        </div>
-      </div>
-    @empty
-      <h3 class="text-center">No Logs to display.</h3>
-    @endforelse
-  </div>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <h3 class="text-center">No Logs to display.</h3>
+        @endforelse
+    </div>
 
 @endsection
 
 @section('after_scripts')
-  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/styles/default.min.css">
-  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js"></script>
-  <script>hljs.initHighlightingOnLoad();</script>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/styles/default.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
 @endsection
+
+@push('after_scripts')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/9.1.1/jsoneditor.min.css" />
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jsoneditor/9.1.1/jsoneditor.min.js"></script>
+    <script>
+        let container, jsonString, options, editor;
+    </script>
+@endpush
+
+@forelse($logs as $key => $log)
+    @push('after_scripts')
+        <script>
+            container = document.getElementById('jsoneditor-{{ $key }}');
+            jsonString = @json($log['text']);
+
+            options = {
+                onChange: function() {
+                    const hiddenField = document.getElementById('{{ $key }}');
+                    hiddenField.value = window['editor_{{ $key }}'].getText();
+                },
+                modes: ['form', 'tree', 'code'],
+            };
+
+            window['editor_{{ $key }}'] = new JSONEditor(container, options, JSON.parse(jsonString));
+            document.getElementById('{{ $key }}').value = window['editor_{{ $key }}'].getText();
+        </script>
+    @endpush
+@empty
+@endforelse
